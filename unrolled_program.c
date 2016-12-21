7dadf24d068571425ea45943852a11f1d2709c1f
YamaMasaya <as-i-you-11t4071y@hotmail.co.jp>
2016-12-21 12:13:32 +0900

    Add files via upload

---
+++ unrolled_program.c
@@ -0,0 +1,89 @@
+#include <stdio.h>
+
+void main(void)
+{
+	int array1[100];
+	int array2[100];
+	int roop,j;
+
+		if(array[0] < 500){
+			for(j = 0; j < 60; j++){
+				if(array[j] < 100){
+					printf("%d\n", array[j]);
+				}
+				else{
+					printf("%d\n", array[0]);
+				}
+			}
+		}
+		else if(array[i] < 100){
+			
+		}
+		else{
+			
+		}
+		if(array[0] < 500){
+			for(j = 0; j < 60; j++){
+				if(array[j] < 100){
+					printf("%d\n", array[j]);
+				}
+				else{
+					printf("%d\n", array[0]);
+				}
+			}
+		}
+		else if(array[i] < 100){
+			
+		}
+		else{
+			
+		}
+		if(array[0] < 500){
+			for(j = 0; j < 60; j++){
+				if(array[j] < 100){
+					printf("%d\n", array[j]);
+				}
+				else{
+					printf("%d\n", array[0]);
+				}
+			}
+		}
+		else if(array[i] < 100){
+			
+		}
+		else{
+			
+		}
+		if(array[0] < 500){
+			for(j = 0; j < 60; j++){
+				if(array[j] < 100){
+					printf("%d\n", array[j]);
+				}
+				else{
+					printf("%d\n", array[0]);
+				}
+			}
+		}
+		else if(array[i] < 100){
+			
+		}
+		else{
+			
+		}
+		if(array[0] < 500){
+			for(j = 0; j < 60; j++){
+				if(array[j] < 100){
+					printf("%d\n", array[j]);
+				}
+				else{
+					printf("%d\n", array[0]);
+				}
+			}
+		}
+		else if(array[i] < 100){
+			
+		}
+		else{
+			
+		}
+}
