cd9a9b609a7ab7e2894f89263f46200a05b66417
YamaMasaya <as-i-you-11t4071y@hotmail.co.jp>
2016-12-26 22:09:07 +0900

    Update unrolled_program.c
    多重ループ、３次元配列展開後

--- unrolled_program.c
+++ unrolled_program.c
@@ -4,70 +4,297 @@ void main(void)
 {
 	int array1[100];
 	int array2[100];
-	int roop,j;
+	int vertex2[100][100];
+	int vertex3[100][100];
+	int switch_vertex3[]
+	int roop,j,tmp1,tmp2,tmp3,tmp4,tmp5; 
+
+	
+
+
+
 
 		if(array[0] < 500){
-			for(j = 0; j < 60; j++){
-				if(array[j] < 100){
-					printf("%d\n", array[j]);
+				if(array[0] < 100){
+					printf("%d\n", array[0]);
 				}
 				else{
 					printf("%d\n", array[0]);
 				}
-			}
+				if(array[1] < 100){
+					printf("%d\n", array[1]);
+				}
+				else{
+					printf("%d\n", array[0]);
+				}
+				if(array[2] < 100){
+					printf("%d\n", array[2]);
+				}
+				else{
+					printf("%d\n", array[0]);
+				}
+				if(array[3] < 100){
+					printf("%d\n", array[3]);
+				}
+				else{
+					printf("%d\n", array[0]);
+				}
+				if(array[4] < 100){
+					printf("%d\n", array[4]);
+				}
+				else{
+					printf("%d\n", array[0]);
+				}
 		}
-		else if(array[i] < 100){
+		else if(array[0] < 100){
 			
 		}
 		else{
 			
 		}
 		if(array[1] < 500){
-			for(j = 0; j < 60; j++){
-				if(array[j] < 100){
-					printf("%d\n", array[j]);
+				if(array[0] < 100){
+					printf("%d\n", array[0]);
 				}
 				else{
 					printf("%d\n", array[1]);
 				}
-			}
+				if(array[1] < 100){
+					printf("%d\n", array[1]);
+				}
+				else{
+					printf("%d\n", array[1]);
+				}
+				if(array[2] < 100){
+					printf("%d\n", array[2]);
+				}
+				else{
+					printf("%d\n", array[1]);
+				}
+				if(array[3] < 100){
+					printf("%d\n", array[3]);
+				}
+				else{
+					printf("%d\n", array[1]);
+				}
+				if(array[4] < 100){
+					printf("%d\n", array[4]);
+				}
+				else{
+					printf("%d\n", array[1]);
+				}
 		}
-		else if(array[i] < 100){
+		else if(array[1] < 100){
 			
 		}
 		else{
 			
 		}
 		if(array[2] < 500){
-			for(j = 0; j < 60; j++){
-				if(array[j] < 100){
-					printf("%d\n", array[j]);
+				if(array[0] < 100){
+					printf("%d\n", array[0]);
 				}
 				else{
 					printf("%d\n", array[2]);
 				}
-			}
+				if(array[1] < 100){
+					printf("%d\n", array[1]);
+				}
+				else{
+					printf("%d\n", array[2]);
+				}
+				if(array[2] < 100){
+					printf("%d\n", array[2]);
+				}
+				else{
+					printf("%d\n", array[2]);
+				}
+				if(array[3] < 100){
+					printf("%d\n", array[3]);
+				}
+				else{
+					printf("%d\n", array[2]);
+				}
+				if(array[4] < 100){
+					printf("%d\n", array[4]);
+				}
+				else{
+					printf("%d\n", array[2]);
+				}
 		}
-		else if(array[i] < 100){
+		else if(array[2] < 100){
 			
 		}
 		else{
 			
 		}
 		if(array[3] < 500){
-			for(j = 0; j < 60; j++){
-				if(array[j] < 100){
-					printf("%d\n", array[j]);
+				if(array[0] < 100){
+					printf("%d\n", array[0]);
 				}
 				else{
 					printf("%d\n", array[3]);
 				}
-			}
+				if(array[1] < 100){
+					printf("%d\n", array[1]);
+				}
+				else{
+					printf("%d\n", array[3]);
+				}
+				if(array[2] < 100){
+					printf("%d\n", array[2]);
+				}
+				else{
+					printf("%d\n", array[3]);
+				}
+				if(array[3] < 100){
+					printf("%d\n", array[3]);
+				}
+				else{
+					printf("%d\n", array[3]);
+				}
+				if(array[4] < 100){
+					printf("%d\n", array[4]);
+				}
+				else{
+					printf("%d\n", array[3]);
+				}
 		}
-		else if(array[i] < 100){
+		else if(array[3] < 100){
 			
 		}
 		else{
 			
 		}
+		if(array[4] < 500){
+				if(array[0] < 100){
+					printf("%d\n", array[0]);
+				}
+				else{
+					printf("%d\n", array[4]);
+				}
+				if(array[1] < 100){
+					printf("%d\n", array[1]);
+				}
+				else{
+					printf("%d\n", array[4]);
+				}
+				if(array[2] < 100){
+					printf("%d\n", array[2]);
+				}
+				else{
+					printf("%d\n", array[4]);
+				}
+				if(array[3] < 100){
+					printf("%d\n", array[3]);
+				}
+				else{
+					printf("%d\n", array[4]);
+				}
+				if(array[4] < 100){
+					printf("%d\n", array[4]);
+				}
+				else{
+					printf("%d\n", array[4]);
+				}
+		}
+		else if(array[4] < 100){
+			
+		}
+		else{
+			
+		}
+			if(vertex2[0][0] < 3){
+    			printf("2次元配列のテスト\n");
+    		}
+			if(vertex2[0][1] < 3){
+    			printf("2次元配列のテスト\n");
+    		}
+			if(vertex2[0][2] < 3){
+    			printf("2次元配列のテスト\n");
+    		}
+			if(vertex2[0][3] < 3){
+    			printf("2次元配列のテスト\n");
+    		}
+			if(vertex2[0][4] < 3){
+    			printf("2次元配列のテスト\n");
+    		}
+
+  				if(vertex3[0][0][0] < 40){
+    				printf("3次元配列のテスト\n");
+    			}
+  				if(vertex3[0][0][1] < 40){
+    				printf("3次元配列のテスト\n");
+    			}
+  				if(vertex3[0][0][2] < 40){
+    				printf("3次元配列のテスト\n");
+    			}
+  				if(vertex3[0][0][3] < 40){
+    				printf("3次元配列のテスト\n");
+    			}
+  				if(vertex3[0][0][4] < 40){
+    				printf("3次元配列のテスト\n");
+    			}
+  				if(vertex3[0][1][0] < 40){
+    				printf("3次元配列のテスト\n");
+    			}
+  				if(vertex3[0][1][1] < 40){
+    				printf("3次元配列のテスト\n");
+    			}
+  				if(vertex3[0][1][2] < 40){
+    				printf("3次元配列のテスト\n");
+    			}
+  				if(vertex3[0][1][3] < 40){
+    				printf("3次元配列のテスト\n");
+    			}
+  				if(vertex3[0][1][4] < 40){
+    				printf("3次元配列のテスト\n");
+    			}
+  				if(vertex3[0][2][0] < 40){
+    				printf("3次元配列のテスト\n");
+    			}
+  				if(vertex3[0][2][1] < 40){
+    				printf("3次元配列のテスト\n");
+    			}
+  				if(vertex3[0][2][2] < 40){
+    				printf("3次元配列のテスト\n");
+    			}
+  				if(vertex3[0][2][3] < 40){
+    				printf("3次元配列のテスト\n");
+    			}
+  				if(vertex3[0][2][4] < 40){
+    				printf("3次元配列のテスト\n");
+    			}
+  				if(vertex3[0][3][0] < 40){
+    				printf("3次元配列のテスト\n");
+    			}
+  				if(vertex3[0][3][1] < 40){
+    				printf("3次元配列のテスト\n");
+    			}
+  				if(vertex3[0][3][2] < 40){
+    				printf("3次元配列のテスト\n");
+    			}
+  				if(vertex3[0][3][3] < 40){
+    				printf("3次元配列のテスト\n");
+    			}
+  				if(vertex3[0][3][4] < 40){
+    				printf("3次元配列のテスト\n");
+    			}
+  				if(vertex3[0][4][0] < 40){
+    				printf("3次元配列のテスト\n");
+    			}
+  				if(vertex3[0][4][1] < 40){
+    				printf("3次元配列のテスト\n");
+    			}
+  				if(vertex3[0][4][2] < 40){
+    				printf("3次元配列のテスト\n");
+    			}
+  				if(vertex3[0][4][3] < 40){
+    				printf("3次元配列のテスト\n");
+    			}
+  				if(vertex3[0][4][4] < 40){
+    				printf("3次元配列のテスト\n");
+    			}
+
+
 }
